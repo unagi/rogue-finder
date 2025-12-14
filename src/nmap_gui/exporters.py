0688fc393019f4a3895899e64b700e15fe671502
unagi <ray@ymgch.org>
2025-12-14 06:05:31 +0000

    Remove legacy exporters wrapper

--- src/nmap_gui/exporters.py
+++
@@ -1,6 +0,0 @@
-"""Backward-compatible exports for GUI exporter helpers."""
-from __future__ import annotations
-
-from .infrastructure.exporters import export_csv, export_json
-
-__all__ = ["export_csv", "export_json"]
